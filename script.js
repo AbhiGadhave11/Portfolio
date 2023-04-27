1574ab8fdf5d6276a5d85f7c2fcbfab551e0b6b5
Abhijit Kailas Gadhave <abhigadhave97@gmail.com>
2023-04-27 10:02:49 +0530

    About updated

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const aboutLink = document.querySelector('a[href="#about"]');
 
 aboutLink.addEventListener('click', (event) => {
-  event.preventDefault(); // prevent page from reloading
-  const aboutSection = document.querySelector('#about');
-  aboutSection.scrollIntoView({ behavior: 'smooth' }); // scroll smoothly to section
+    event.preventDefault(); // prevent page from reloading
+    const aboutSection = document.querySelector('#about');
+    aboutSection.scrollIntoView({ behavior: 'smooth' }); // scroll smoothly to section
 });
 
-const words = ["Student", "Engineer", "Designer","Developer"];
-const wcolors = ["yellow", "orange", "yellow","orange"];
+const words = ["Student!", "Engineer!", "Designer!", "Developer!"];
+const wcolors = ["yellow", "orange", "yellow", "orange"];
 const word = document.querySelector('.word')
-word.innerHTML=words[0]
-word.style.color=wcolors[0]
+word.innerHTML = words[0]
+word.style.color = wcolors[0]
 var index = 1
 
-setInterval(() =>{
-    word.innerHTML=words[index];
-    word.style.color=wcolors[index]
-    index = (index+1)%4;
-},1500)
+setInterval(() => {
+    word.innerHTML = words[index];
+    word.style.color = wcolors[index]
+    index = (index + 1) % 4;
+}, 1500)
+
+
+
+const aboutwords = ["I Like Travelling!", "I like Music!", "I like Cricket!", "I like RCB!"];
+const aboutin = document.querySelector('.aboutin')
+aboutin.innerHTML = aboutwords[0]
+var ind = 1
+
+setInterval(() => {
+    aboutin.innerHTML = aboutwords[ind];
+    ind = (ind + 1) % 4;
+}, 1300)
 
 
 
