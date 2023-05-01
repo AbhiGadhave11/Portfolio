6e3ccf5fa863d035950c1c0eecb11eec14a68c8a
Abhijit Kailas Gadhave <abhigadhave97@gmail.com>
2023-05-01 15:39:40 +0530

    Header Update

--- script.js
+++ script.js
@@ -7,17 +7,42 @@ aboutLink.addEventListener('click', (event) => {
 });
 
 const words = ["Student!", "Engineer!", "Designer!", "Developer!"];
-const wcolors = ["yellow", "orange", "yellow", "orange"];
+const wcolors = ["yellow", "orange"];
 const word = document.querySelector('.word')
-word.innerHTML = words[0]
-word.style.color = wcolors[0]
-var index = 1
+// word.innerHTML = words[0]
+// word.style.color = wcolors[0]
+// var index = 1
 
-setInterval(() => {
-    word.innerHTML = words[index];
-    word.style.color = wcolors[index]
-    index = (index + 1) % 4;
-}, 1500)
+// setInterval(() => {
+//     word.innerHTML = words[index];
+//     word.style.color = wcolors[index]
+//     index = (index + 1) % 4;
+// }, 1500)
+
+var ch = '|'
+var index=0, letter=0;
+var cnt = 0, _size = 3;
+word.innerHTML = ch;
+word.style.color = wcolors[1];
+setInterval(()=>{
+    if(letter === words[index].length && cnt){
+        cnt = (cnt + 1)%_size;
+    }
+    else if(letter < words[index].length){
+        if(letter != words[index].length-1) word.textContent = word.textContent.slice(0,-1) + words[index][letter++] + '|';
+        else word.textContent = word.textContent.slice(0,-1) + words[index][letter++];
+        cnt=1;
+    }
+    else{
+        word.textContent='';
+        letter=0;
+        word.style.color = wcolors[index%2];
+        index = (index+1)%words.length;
+    }
+},600);
+
+
+
 
 
 
@@ -35,6 +60,12 @@ function scrollToSection(sectionId) {
     var section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: "smooth" });
   }
+
+
+
+
+
+
 
 
 
